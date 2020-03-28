898f5a07d8008b43da94a85ab6158f8b756a3bda
pitufocabeza <pitufocabeza@gmail.com>
2020-03-28 10:07:31 +0100

    Changed font size

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -72,17 +72,17 @@ export const IndexPageTemplate = ({
               <div className="content">
                 <div className="content">
                   <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
+                    <h3 className="title">{mainpitch.title}</h3>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
+                    <h4 className="subtitle">{mainpitch.description}</h4>
                   </div>
                 </div>
                 <div className="columns">
                   <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
+                    <h4 className="has-text-weight-semibold is-size-2">
                       {heading}
-                    </h3>
+                    </h4>
                     <p>{description}</p>
                   </div>
                 </div>
